2b276482e2a45727e5cbd01580dc6fabacfaf62a
uday chauhan <chauhan.s.uday26@gmail.com>
2020-04-09 01:23:19 +0530

    LeetCode solved examples added

---
+++ src/main/java/thirty/day/covid/LeetCodeChallenges/ProfitLossInShares.java
@@ -0,0 +1,33 @@
+package thirty.day.covid.LeetCodeChallenges;
+
+public class ProfitLossInShares {
+
+	public static void main(String[] args) {
+		ProfitLossInShares profl = new ProfitLossInShares();
+		int[] prices = { 7, 1, 5, 3, 6, 4 };
+		int res = profl.maxProfit(prices);
+		System.out.println("Max profit is " + res);
+	}
+
+	public int maxProfit(int[] prices) {
+		int max = 0;
+		int buy = 0;
+		int sell = 0;
+		for (int i = 0; i < prices.length - 1; i++) {
+			if (prices[i] < prices[i + 1]) {
+				sell = i + 1;
+			} else {
+				if (sell != 0) {
+					max = max + prices[sell] - prices[buy];
+					sell = 0;
+
+				}
+				buy = i + 1;
+			}
+		}
+		if (sell != 0) {
+			max = max + prices[sell] - prices[buy];
+		}
+		return max;
+	}
+}
