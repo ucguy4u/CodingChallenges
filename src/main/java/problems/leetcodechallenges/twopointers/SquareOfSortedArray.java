9331bbfd089c38966dcdf234ca0b402bcc2344c2
uday chauhan <chauhan.s.uday26@gmail.com>
2020-04-09 02:49:02 +0530

    Min Stack (LeetCode Challenge Day 10)

---
+++ src/main/java/problems/leetcodechallenges/twopointers/SquareOfSortedArray.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package problems.leetcodechallenges.twopointers;
+
+import java.util.Arrays;
+
+/**
+ * @author ucguy4u
+ *
+ *         Given an array of integers A sorted in non-decreasing order, return
+ *         an array of the squares of each number, also in sorted non-decreasing
+ *         order.
+ * 
+ * 
+ * 
+ *         Example 1:
+ * 
+ *         Input: [-4,-1,0,3,10] Output: [0,1,9,16,100]
+ * 
+ *         Example 2:
+ * 
+ *         Input: [-7,-3,2,3,11] Output: [4,9,9,49,121]
+ * 
+ * 
+ * 
+ *         Note:
+ * 
+ *         1 <= A.length <= 10000 -10000 <= A[i] <= 10000 A is sorted in
+ *         non-decreasing order.
+ *
+ * 
+ * 
+ */
+public class SquareOfSortedArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SquareOfSortedArray obj = new SquareOfSortedArray();
+		int[] A = { -4, -1, 0, 3, 10 };
+		int[] ans = obj.sortedSquares(A);
+		System.out.println("Input :" + Arrays.toString(ans));
+		System.out.println("Output :" + Arrays.toString(ans));
+
+	}
+
+	public int[] sortedSquares(int[] A) {
+		int n = A.length;
+		int[] result = new int[n];
+		int i = 0;
+		int j = n - 1;
+		for (int p = n - 1; p >= 0; p--) {
+			// sorting the numbers as start compare with from first and last element in the
+			// array
+			if (Math.abs(A[i]) > Math.abs(A[j])) {
+				// store the square of number
+				result[p] = A[i] * A[i];
+				i++;
+			} else {
+				// store the square of number
+				result[p] = A[j] * A[j];
+				j--;
+			}
+		}
+		return result;
+	}
+
+}
---
+++ src/main/java/thirty/day/covid/LeetCodeChallenges/BackspaceStringCompare.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package thirty.day.covid.LeetCodeChallenges;
+
+/**
+ * @author ucguy4u
+ *
+ *         Given two strings S and T, return if they are equal when both are
+ *         typed into empty text editors. # means a backspace character.
+ * 
+ *         Example 1:
+ * 
+ *         Input: S = "ab#c", T = "ad#c" Output: true Explanation: Both S and T
+ *         become "ac".
+ * 
+ *         Example 2:
+ * 
+ *         Input: S = "ab##", T = "c#d#" Output: true Explanation: Both S and T
+ *         become "".
+ * 
+ *         Example 3:
+ * 
+ *         Input: S = "a##c", T = "#a#c" Output: true Explanation: Both S and T
+ *         become "c".
+ * 
+ *         Example 4:
+ * 
+ *         Input: S = "a#c", T = "b" Output: false Explanation: S becomes "c"
+ *         while T becomes "b".
+ * 
+ *         Note:
+ * 
+ *         1 <= S.length <= 200 1 <= T.length <= 200 S and T only contain
+ *         lowercase letters and '#' characters.
+ * 
+ *         Follow up:
+ * 
+ *         Can you solve it in O(N) time and O(1) space?
+ *
+ * 
+ * 
+ */
+public class BackspaceStringCompare {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String S = "ab#c", T = "ad#c";
+		BackspaceStringCompare obj = new BackspaceStringCompare();
+		System.out.println(obj.backspaceCompare(S, T));
+	}
+
+	public boolean backspaceCompare(String S, String T) {
+		int spointer = S.length() - 1;
+		int tpointer = T.length() - 1;
+
+		int sskip = 0;
+		int tskip = 0;
+
+		while (spointer >= 0 || tpointer >= 0) {
+			while (spointer >= 0) {
+				if (S.charAt(spointer) == '#') {
+					sskip += 1;
+					spointer -= 1;
+				} else if (sskip > 0) {
+					spointer -= 1;
+					sskip -= 1;
+				} else {
+					break;
+				}
+			}
+			while (tpointer >= 0) {
+				if (T.charAt(tpointer) == '#') {
+					tskip += 1;
+					tpointer -= 1;
+				} else if (tskip > 0) {
+					tpointer -= 1;
+					tskip -= 1;
+				} else {
+					break;
+				}
+			}
+			if (spointer >= 0 && tpointer >= 0 && S.charAt(spointer) != T.charAt(tpointer)) {
+				return false;
+			}
+			if ((spointer >= 0) != (tpointer >= 0)) {
+				return false;
+			}
+			spointer -= 1;
+			tpointer -= 1;
+
+		}
+		return true;
+
+	}
+}
