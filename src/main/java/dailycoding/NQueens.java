96e74536b0df3a04584451372702a017e96822e1
Uday Chauhan <chauhan.s.uday26@gmail.com>
2020-10-18 05:07:31 +0000

    saving the work from gitpod

--- src/main/java/dailycoding/NQueens.java
+++ src/main/java/dailycoding/NQueens.java
@@ -1,8 +1,38 @@
 package dailycoding;
 
+/**
+ * You have an N by N board. Write a function that returns the number of
+ * possible arrangements of the board where N queens can be placed on the board
+ * without threatening each other, i.e. no two queens share the same row,
+ * column, or diagonal.
+ * 
+ * @author ucguy4u
+ */
+
 public class NQueens {
-    public static void main (String args[]){
-        System.out.println("Hello");
+    // define the numbers of queens
+    final static int n = 4;
+
+    public static void main(final String args[]) {
+        NQueens obj = new NQueens();
+        int[][] board = new int[n][n];
+        obj.processBoard(board, 0);
+        obj.isValid();
+
     }
 
+    private boolean processBoard(int[][] board, int c) {
+        if (c >= n)
+            return true;
+        for (int i = 0; i < n; i++) {
+
+        }
+        return false;
+
+    }
+
+    private boolean isValid() {
+        System.out.println(n);
+        return true;
+    }
 }
